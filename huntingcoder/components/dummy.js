326db766bc0e52c69a8d2c75bd3c3ff3f92ccf3d
Haris Ali Khan <haris989@users.noreply.github.com>
2022-02-11 14:09:07 +0530

    Tutorial 12

---
+++ huntingcoder/components/dummy.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Dummy = () => {
+  return (
+      <>
+      <style jsx>
+          {`
+          .dummy{
+              background: yellow;
+          }
+          `}
+      </style>
+    <div className='dummy'>I am dummy dummy</div>
+    </>
+  )
+}
+
+export default Dummy

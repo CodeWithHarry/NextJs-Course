87af461988f9ceca6b5e7de79585dd0fd5768a2c
Haris Ali Khan <haris989@users.noreply.github.com>
2022-02-16 18:29:33 +0530

    Tutorial 17

--- huntingcoder/pages/api/index.js
+++ huntingcoder/pages/api/index.js
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  // console.log(res)
-  res.status(200).json({ name: 'Harry Bhai' })
-}
+    // console.log(res)
+    res.status(200).json({ name: 'This is my homepage' })
+  }
+  

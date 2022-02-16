0d61d2411e44d258980a34ed1c5225b338e12b8a
Haris Ali Khan <haris989@users.noreply.github.com>
2022-02-16 09:56:29 +0530

    Tutorial 15

--- huntingcoder/pages/blog.js
+++ huntingcoder/pages/blog.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import styles from '../styles/Blog.module.css' 
+import Link from 'next/link';
 
 const Blog = () => {
-  return <div>This is Blog home page</div>;
+  return <div className={styles.container}>
+    <main className={styles.main}>
+  <div>
+    <Link href={'/blogpost/learn-javascript'}>
+    <h3 className={styles.blogItemh3}>How to learn JavaScript in 2022?</h3></Link>
+    <p>JavaScript is the language used to design logic for the web</p>
+  </div>
+  <div className="blogItem">
+    <h3>How to learn JavaScript in 2022?</h3>
+    <p>JavaScript is the language used to design logic for the web</p>
+  </div>
+  <div className="blogItem">
+    <h3>How to learn JavaScript in 2022?</h3>
+    <p>JavaScript is the language used to design logic for the web</p>
+  </div>
+  </main>
+</div>
 };
 
 export default Blog;
--- huntingcoder/pages/blogpost/[slug].js
+++ huntingcoder/pages/blogpost/[slug].js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { useRouter } from 'next/router'
+import styles from '../../styles/BlogPost.module.css' 
 
 const slug = () => {
     const router = useRouter();
     const { slug } = router.query;
-    return <div>{slug}</div>;
+    return <div className={styles.container}>
+        <main className={styles.main}>
+        <h1>Title of the page {slug}</h1>
+        <hr />
+        <div>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem nulla repudiandae sint facilis, sunt corrupti numquam id illo. Ut deserunt animi iste voluptatum!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus maxime rem earum repudiandae, cum possimus quae assumenda nulla culpa. Odit architecto repellendus non, unde recusandae placeat nisi perferendis quod nesciunt! Dolorum sapiente et sint consequuntur earum blanditiis iusto reprehenderit molestiae quia eligendi? Exercitationem, officia nobis!
+        </div>
+        </main>
+    </div>;
 };
 
 export default slug;

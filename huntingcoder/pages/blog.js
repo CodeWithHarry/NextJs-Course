8df1897c89019329a1d4fce7f351ae3e151217b0
Haris Ali Khan <haris989@users.noreply.github.com>
2022-02-16 13:16:59 +0530

    Tutorial 16

--- huntingcoder/pages/blog.js
+++ huntingcoder/pages/blog.js
@@ -2,6 +2,9 @@ import React from 'react';
 import styles from '../styles/Blog.module.css' 
 import Link from 'next/link';
 
+// Step 1: Collect all the files from blogdata directory
+// Step 2: Iterate through the and Display them
+
 const Blog = () => {
   return <div className={styles.container}>
     <main className={styles.main}>
--- huntingcoder/pages/blogpost/[slug].js
+++ huntingcoder/pages/blogpost/[slug].js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useRouter } from 'next/router'
 import styles from '../../styles/BlogPost.module.css' 
 
+// Step 1: Find the file corresponding to the slug
+// Step 2: Populate them inside the page
 const slug = () => {
     const router = useRouter();
     const { slug } = router.query;

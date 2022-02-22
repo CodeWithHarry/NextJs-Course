ea45bdabbe634255298559a30b880cfb0181ff64
Haris Ali Khan <haris989@users.noreply.github.com>
2022-02-22 14:48:43 +0530

    Tutorial 19

--- huntingcoder/pages/api/blogs.js
+++ huntingcoder/pages/api/blogs.js
@@ -1,9 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import * as fs from 'fs';
 
-export default function handler(req, res) {
-  fs.readdir("blogdata", (err, data)=>{
-    console.log(data)
-    res.status(200).json(data)
-  })
+export default async function handler(req, res) {
+
+    let data = await fs.promises.readdir("blogdata");
+    let myfile; 
+    let allBlogs = [];
+      for (let index = 0; index < data.length; index++) {
+        const item = data[index];
+          console.log(item)
+          myfile = await fs.promises.readFile(('blogdata/' + item), 'utf-8') 
+          allBlogs.push(JSON.parse(myfile))
+      }
+    res.status(200).json(allBlogs)
+  // fs.promises.readdir("blogdata", (err, data)=>{
+  //   console.log(data)
+  //   let allBlogs = [];
+  //   data.forEach((item)=>{
+  //     console.log(item)
+  //     fs.readFile(('blogdata/' + item), (d)=>{
+  //       allBlogs.push(d)
+  //     })
+  //   })
+  // })
+  // res.status(200).json(allBlogs)
 }
--- huntingcoder/pages/blog.js
+++ huntingcoder/pages/blog.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '../styles/Blog.module.css' 
 import Link from 'next/link';
 
@@ -6,21 +6,25 @@ import Link from 'next/link';
 // Step 2: Iterate through the and Display them
 
 const Blog = () => {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(()=>{
+    console.log("useeffect is running");
+    fetch('http://localhost:3000/api/blogs').then((a)=>{
+      return a.json();})
+      .then((parsed)=>{ 
+        console.log(parsed)
+        setBlogs(parsed)
+    })
+  }, [])
   return <div className={styles.container}>
-    <main className={styles.main}>
-  <div>
-    <Link href={'/blogpost/learn-javascript'}>
-    <h3 className={styles.blogItemh3}>How to learn JavaScript in 2022?</h3></Link>
-    <p>JavaScript is the language used to design logic for the web</p>
-  </div>
-  <div className="blogItem">
-    <h3>How to learn JavaScript in 2022?</h3>
-    <p>JavaScript is the language used to design logic for the web</p>
-  </div>
-  <div className="blogItem">
-    <h3>How to learn JavaScript in 2022?</h3>
-    <p>JavaScript is the language used to design logic for the web</p>
-  </div>
+    <main className={styles.main}> 
+      {blogs.map((blogitem)=>{
+        return <div key={blogitem.slug}>
+        <Link href={`/blogpost/${blogitem.slug}`}>
+        <h3 className={styles.blogItemh3}>{blogitem.title}</h3></Link>
+        <p className={styles.blogItemp}>{blogitem.content.substr(0, 140)}...</p>
+      </div>
+      })} 
   </main>
 </div>
 };

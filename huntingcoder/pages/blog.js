b578836b8af177056a6c6e3336d13705036f27ff
Haris Ali Khan <haris989@users.noreply.github.com>
2022-02-23 08:23:34 +0530

    Tutorial 20

--- huntingcoder/pages/blog.js
+++ huntingcoder/pages/blog.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import styles from '../styles/Blog.module.css' 
+import styles from '../styles/Blog.module.css'
 import Link from 'next/link';
 
 // Step 1: Collect all the files from blogdata directory
@@ -7,26 +7,26 @@ import Link from 'next/link';
 
 const Blog = () => {
   const [blogs, setBlogs] = useState([]);
-  useEffect(()=>{
+  useEffect(() => {
     console.log("useeffect is running");
-    fetch('http://localhost:3000/api/blogs').then((a)=>{
-      return a.json();})
-      .then((parsed)=>{ 
-        console.log(parsed)
-        setBlogs(parsed)
+    fetch('http://localhost:3000/api/blogs').then((a) => {
+      return a.json();
     })
+      .then((parsed) => {
+        setBlogs(parsed)
+      })
   }, [])
   return <div className={styles.container}>
-    <main className={styles.main}> 
-      {blogs.map((blogitem)=>{
+    <main className={styles.main}>
+      {blogs.map((blogitem) => {
         return <div key={blogitem.slug}>
-        <Link href={`/blogpost/${blogitem.slug}`}>
-        <h3 className={styles.blogItemh3}>{blogitem.title}</h3></Link>
-        <p className={styles.blogItemp}>{blogitem.content.substr(0, 140)}...</p>
-      </div>
-      })} 
-  </main>
-</div>
+          <Link href={`/blogpost/${blogitem.slug}`}>
+            <h3 className={styles.blogItemh3}>{blogitem.title}</h3></Link>
+          <p className={styles.blogItemp}>{blogitem.content.substr(0, 140)}...</p>
+        </div>
+      })}
+    </main>
+  </div>
 };
 
 export default Blog;
--- huntingcoder/pages/blogpost/[slug].js
+++ huntingcoder/pages/blogpost/[slug].js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { useRouter } from 'next/router'
 import styles from '../../styles/BlogPost.module.css' 
 
 // Step 1: Find the file corresponding to the slug
 // Step 2: Populate them inside the page
 const slug = () => {
+    const [blog, setBlog] = useState();
     const router = useRouter();
-    const { slug } = router.query;
+    useEffect(() => {
+        if (!router.isReady) return;
+        const { slug } = router.query;
+        fetch(`http://localhost:3000/api/getblog?slug=${slug}`).then((a) => {
+            return a.json();
+          })
+            .then((parsed) => {
+              setBlog(parsed)
+            })
+    }, [router.isReady])
+    
+    
     return <div className={styles.container}>
         <main className={styles.main}>
-        <h1>Title of the page {slug}</h1>
+        <h1>{blog && blog.title}</h1>
         <hr />
         <div>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem nulla repudiandae sint facilis, sunt corrupti numquam id illo. Ut deserunt animi iste voluptatum!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus maxime rem earum repudiandae, cum possimus quae assumenda nulla culpa. Odit architecto repellendus non, unde recusandae placeat nisi perferendis quod nesciunt! Dolorum sapiente et sint consequuntur earum blanditiis iusto reprehenderit molestiae quia eligendi? Exercitationem, officia nobis!
-        </div>
+            {blog && blog.content}
+            </div>
         </main>
     </div>;
 };

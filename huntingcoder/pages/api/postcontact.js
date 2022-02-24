28a9ef10b43dcdb66458a6f53ea27c41219127a0
Haris Ali Khan <haris989@users.noreply.github.com>
2022-02-24 19:23:06 +0530

    Tutorial 25

--- huntingcoder/pages/api/postcontact.js
+++ huntingcoder/pages/api/postcontact.js
@@ -1,8 +1,14 @@
-export default function handler(req, res) {
+import * as fs from 'fs';
+export default async function  handler(req, res) {
     if (req.method === 'POST') {
-      // Process a POST request
+      // Process a POST request 
+      let data = await fs.promises.readdir('contactdata');
+      fs.promises.writeFile(`contactdata/${data.length+1}.json`, JSON.stringify(req.body))
+      res.status(200).json(req)
     } else {
       // Handle any other HTTP method
       res.status(200).json(["allBlogs"])
+    //   name, email, desc, phone
+
     }
   }
